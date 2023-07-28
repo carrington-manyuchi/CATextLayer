e5b1de9159ae79811a7371681dfc44e63f64ac5d
Carrington <Manyuchi>
2023-07-28 08:32:00 +0200

    CATextLayer that is showing text inside a view

--- CATextLayer/ViewController.swift
+++ CATextLayer/ViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    
+     let myLayer: CATextLayer = {
+         let text = "vdwjjkrgebfdla"
+         let font: CFString = "Noteworthy-Light" as CFString
+         let layer = CATextLayer()
+         layer.foregroundColor = UIColor.brown.cgColor
+         layer.isWrapped  = true
+         layer.alignmentMode = .center
+         layer.contentsScale = UIScreen.main.scale
+         layer.string = text
+         layer.font = CTFontCreateWithName( font, 25, nil)
+         return layer
+    }()
+    
+    private var myView: UIView = {
+        let view = UIView()
+        //view.translatesAutoresizingMaskIntoConstraints = false
+        view.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        view.backgroundColor = .black
+        
+        return view
+    }()
+    
+    // first loading func
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(myView)
+        myView.center = self.view.center
+        myLayer.frame = myView.bounds
+        myView.layer.addSublayer(myLayer)
+        
     }
 
 
